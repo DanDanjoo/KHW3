899d1585ba6bcd7a059b86d8eff01533d38ee696
DanDanjoo <murekal94@gmail.com>
2024-05-16 23:10:50 +0900

    완료

---
+++ src/main/kotlin/com/teamsparta/todoregistration/Khw3Application.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.todoregistration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Khw3Application
+
+fun main(args: Array<String>) {
+	runApplication<Khw3Application>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/todoregistration/Khw3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todoregistration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Khw3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
